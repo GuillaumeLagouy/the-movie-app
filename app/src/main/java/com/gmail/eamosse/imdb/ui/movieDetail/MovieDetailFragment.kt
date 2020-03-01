c5c67e7072dc83d0d4df022ca0941ad30b9f06ed
GuillaumeLagouy1 <glagouy.pro@gmail.com>
2020-03-01 11:47:02 +0100

    Similar movies are clickable

--- app/src/main/java/com/gmail/eamosse/imdb/ui/movieDetail/MovieDetailFragment.kt
+++ app/src/main/java/com/gmail/eamosse/imdb/ui/movieDetail/MovieDetailFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.gmail.eamosse.imdb.databinding.FragmentMovieDetailBinding
-import com.gmail.eamosse.imdb.ui.listMovies.MovieAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MovieDetailFragment : Fragment() {
@@ -39,7 +39,8 @@ class MovieDetailFragment : Fragment() {
             getSimilarMovies(MovieDetailFragmentArgs.fromBundle(arguments!!).movieId)
             similar.observe(viewLifecycleOwner, Observer {
                 binding.similarList.adapter = SimilarMoviesAdapter(it){ movie ->
-                    print(movie)
+                    val action = MovieDetailFragmentDirections.actionMovieDetailFragmentToMovieDetailFragment(movie.id)
+                    findNavController().navigate(action)
                 }
             })
         }
